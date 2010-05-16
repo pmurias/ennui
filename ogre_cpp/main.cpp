821d7f5982abbbf4d365789f3b449c1ee5656c7c
pmurias <pawelmurias@gmail.com>
2010-05-16 19:23:40 +0200

    fix the file i accidently cut text from, fix the plugins.cfg

--- ogre_cpp/main.cpp
+++ ogre_cpp/main.cpp
@@ -1,4 +1,100 @@
 /*
  * g++ main.cpp -lOgreMain -lOIS
  */
+#include <OGRE/Ogre.h>
+#include <OIS/OIS.h>
+
+using namespace Ogre;
+
+Root* root;
+RenderWindow* window;
+SceneManager* sceneMgr;
+Camera* camera;
+Viewport* viewPort;
+OIS::Keyboard* keyboard;
+OIS::Mouse*     mouse;
+Real timeDelta;
+Real timeStep;
+
+bool GameStep() {
+    if (keyboard->isKeyDown(OIS::KC_ESCAPE)) return false;
+    return true;
+}
+
+int main(void) {
+    root = new Root;
+
+    ConfigFile cf;
+    cf.load("resources.cfg");
+
+    ConfigFile::SectionIterator seci = cf.getSectionIterator();
+    String secName, typeName, archName;
+    while (seci.hasMoreElements()) {
+        secName = seci.peekNextKey();
+        ConfigFile::SettingsMultiMap *settings = seci.getNext();
+        ConfigFile::SettingsMultiMap::iterator i;
+        for (i = settings->begin(); i != settings->end(); ++i) {
+            typeName = i->first;
+            archName = i->second;
+            ResourceGroupManager::getSingleton().addResourceLocation(
+                archName, typeName, secName);
+        }
+    }
+
+    /* To dziadowskie okienko mozna zastapic pozniej ustawieniami z pliku */
+    if(!root->showConfigDialog()) {
+        delete root;
+        return false;
+    }
+
+    window = root->initialise(true);
+
+    ResourceGroupManager::getSingleton().initialiseAllResourceGroups();
+
+    sceneMgr = root->createSceneManager(ST_GENERIC);
+    camera = sceneMgr->createCamera("MainCamera");
+    viewPort = window->addViewport(camera);
+
+    OIS::ParamList pl;
+    size_t windowHnd = 0;
+    std::ostringstream windowHndStr;
+
+    window->getCustomAttribute("WINDOW", &windowHnd);
+    windowHndStr << windowHnd;
+    pl.insert(std::make_pair(std::string("WINDOW"), windowHndStr.str()));
+
+    OIS::InputManager* inputManager = OIS::InputManager::createInputSystem(pl);
+    keyboard = static_cast<OIS::Keyboard*>(inputManager->createInputObject(OIS::OISKeyboard, true));
+    mouse = static_cast<OIS::Mouse*>(inputManager->createInputObject(OIS::OISMouse, true));
+
+    unsigned int width, height, depth;
+    int top, left;
+    window->getMetrics(width, height, depth, left, top);
+    const OIS::MouseState &ms = mouse->getMouseState(); ms.width = width; ms.height = height;
+
+    bool stop = false;
+    Real lastTime = root->getTimer()->getMilliseconds();
+
+    while (!stop) {
+        keyboard->capture();
+        mouse->capture();
+
+        Real currTime = root->getTimer()->getMilliseconds();
+
+        timeDelta = currTime - lastTime;
+
+        if (!GameStep()) stop = true;
+        root->renderOneFrame();
+
+        lastTime = currTime;
+    }
+
+    inputManager->destroyInputObject(mouse); mouse = 0;
+    inputManager->destroyInputObject(keyboard); keyboard = 0;
+    OIS::InputManager::destroyInputSystem(inputManager); inputManager = 0;
+
+    delete root;
+
+    return 0;
+}
 
